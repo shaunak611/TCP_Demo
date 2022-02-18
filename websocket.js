f5f44edb44cbcbc9cec818ebc7732268a6c3b96b
shaunak611 <98869084+shaunak611@users.noreply.github.com>
2022-02-18 13:32:56 +0530

    second commit

--- websocket.js
+++ websocket.js
@@ -6,38 +6,35 @@ wss.on('connection', function connection(ws,request,client) {
 
   ws.on('message', function message(data) {
 
-    //var msg = JSON.parse(data);
+    //const gps = JSON.parse(data);
 
     const ip = request.socket.remoteAddress;
     console.log('ip=' + ip);
     console.log('req ID = '+request.url);
     console.log(`received: %s `, data);
 
-    ws.send(data)
+    // ws.send(data)
+    const url = request.url
+    const myArray = url.split("/");
+    console.log("url="+myArray[2]);
 
-    function calc(e){
-      if((data%2)==0){
-        let temp = parseInt(e)-100
-        return temp
-      }else{
-        let temp = parseInt(e)+100
-        return temp
-      }
-    }
+    // function calc(e){
+    //   if((data%2)==0){
+    //     let temp = parseInt(e)-100
+    //     return temp
+    //   }else{
+    //     let temp = parseInt(e)+100
+    //     return temp
+    //   }
+    // }
 
-    if(request.url=='/1'){
-      if((data%2)==0){
-        ws.send('Send data for client 1 is Even '+calc(data))
-      }else{
-        ws.send('Send data for client 1 is Odd '+calc(data))
-      }
+    if(resultLength=='0'){
+      gps.save().then(()=>{
+        console.log('GPS data stored...');
+      }).catch(err=>console.log(err))
     }
-    else if(request.url=='/2'){
-      if((data%2)==0){
-        ws.send('Send data for client 1 is Even '+calc(data))
-      }else{
-        ws.send('Send data for client 1 is Odd '+calc(data))
-      }
+    else if(!resultLength=='0'){
+      ws.send('Send data for client '+myArray[2]+' from '+myArray[1]+' is '+ 'Location..!!')
     }
     else{
         ws.send('Send data for another client is '+calc(data))
